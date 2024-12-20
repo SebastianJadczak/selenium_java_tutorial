62ee89245afab101b5ae6f4793d783a0107e095c
sebas <sebastian-jadczak@wp.pl>
2024-12-20 12:45:51 +0100

    add test RegisterTest

---
+++ src/test/java/pl/seleniumdemo/tests/RegisterTest.java
@@ -0,0 +1,12 @@
+package pl.seleniumdemo.tests;
+
+import org.testng.annotations.Test;
+
+
+public class RegisterTest {
+
+    @Test
+    public void testRegister() {
+        System.out.println("Mock");
+    }
+}
