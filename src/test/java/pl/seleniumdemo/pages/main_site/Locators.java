4b5912b75244b372d61214db784c0639e9297d8e
sebas <sebastian-jadczak@wp.pl>
2024-12-23 12:07:21 +0100

    add new locators in main site package

--- src/test/java/pl/seleniumdemo/pages/main_site/Locators.java
+++ src/test/java/pl/seleniumdemo/pages/main_site/Locators.java
@@ -10,9 +10,34 @@ public class Locators {
     public Locators(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
+
     @FindBy(xpath = "//span[text()='Search by Hotel or City Name']")
     protected WebElement searchByHotelSpan;
 
     @FindBy(xpath = "//*[@id='select2-drop']//input")
-    protected WebElement nameCityOrHotel;
+    protected WebElement nameCityOrHotelInput;
+
+    @FindBy(name = "checkin")
+    protected WebElement checkinInput;
+
+    @FindBy(name = "checkout")
+    protected WebElement checkoutInput;
+
+    @FindBy(name = "travellers")
+    protected WebElement travellersTrigger;
+
+    @FindBy(id = "adultPlusBtn")
+    protected WebElement adultPlusButton;
+
+    @FindBy(id = "adultMinusBtn")
+    protected WebElement adultMinusButton;
+
+    @FindBy(id = "childPlusBtn")
+    protected WebElement childPlusButton;
+
+    @FindBy(id = "childMinusBtn")
+    protected WebElement childMinusButton;
+
+    @FindBy(xpath = "//*[@id='hotels']/form/div[5]/button")
+    protected WebElement SearchHotelButton;
 }
