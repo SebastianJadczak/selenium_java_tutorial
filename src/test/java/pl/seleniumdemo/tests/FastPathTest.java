b129732696f0acdb69938e2dbcbe7886e7e29458
sebas <sebastian-jadczak@wp.pl>
2024-12-23 12:06:32 +0100

    refactor test method, add common path

--- src/test/java/pl/seleniumdemo/tests/FastPathTest.java
+++ src/test/java/pl/seleniumdemo/tests/FastPathTest.java
@@ -1,15 +1,14 @@
 package pl.seleniumdemo.tests;
 
 import org.testng.annotations.Test;
-import pl.seleniumdemo.pages.main_site.Page;
 
 
 public class FastPathTest extends BaseTest {
 
+
     @Test
     public void testSearchHotel() {
-        Page page = new Page(driver);
-        driver.get("http://www.kurs-selenium.pl/demo/");
-        page.fillSearchForm();
+        //todo: test data
+        new CommonPath(driver).searchHotel();
     }
 }
